3a2bdeea03d6546a32102736bde447bc0b6fd2eb
Daniela M <Daniela M>
2017-05-01 17:02:27 -0400

    fixed connect.php

--- connect.php
+++ connect.php
@@ -5,15 +5,6 @@ $username   = 'root';
 $password   = '';
 $database   = 'testdb';
 
-// if(!mysql_connect($server, $username,  $password))
-// {
-//     exit('Error: could not establish database connection');
-// }
-// if(!mysql_select_db($database)
-// {
-//     exit('Error: could not select the database');
-// }
-
 $db = new mysqli($server, $username, $password, $database);
 
 ?>
